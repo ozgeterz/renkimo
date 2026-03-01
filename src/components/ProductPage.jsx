65f8418748a1a97dd5a0cff1dfc7db499b25d2c0
ozgeterz <terziozge@hotmail.com>
2026-03-01 11:12:42 +0300

    fixes

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -15,7 +15,7 @@ import filVideo from "../assets/fil.mp4";
 const cities = Object.keys(citiesAndDistricts);
 
 const productVariants = [
-  { id: "mavi-stitch", name: "Mavi Stitch", image: gm3 },
+  // { id: "mavi-stitch", name: "Mavi Stitch", image: gm3 },
   // { id: "pembe-ayıcık", name: "Pembe Ayıcık", image: gm2 },
   { id: "pembe-stitch", name: "Pembe Stitch", image: gm4 },
 ];
